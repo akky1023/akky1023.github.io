0049231f2517212f5ee564cacc7c7862a758b08a
akky_1023 <akky.s.main@gmail.com>
2026-02-15 20:05:22 +0900

    add toggle theme button

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// toggle theme light/dark
+
+const buttonThemeToggle = document.getElementById("buttonThemeToggle");
+const currentTheme =  localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+    document.documentElement.setAttribute("darktheme", "dark")
+}
+
+buttonThemeToggle.addEventListener("click", () => {
+        let theme = "light";
+
+        if (document.documentElement.getAttribute("data-theme") !== "dark") {
+            theme = "dark"
+        }
+
+        document.documentElement.setAttribute("data-theme", theme);
+        localStorage.setItem("theme", theme);
+    }
+);
