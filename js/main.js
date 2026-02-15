72b6b83959e69c6e9a055157dfde86b69480857f
akky_1023 <akky.s.main@gmail.com>
2026-02-16 00:35:29 +0900

    add function: changing theme toggle button icon

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // toggle theme light/dark
 
 const buttonThemeToggle = document.getElementById("buttonThemeToggle");
+const buttonThemeToggleIcon = document.getElementById("buttonThemeToggleIcon");
 const currentTheme =  localStorage.getItem("theme");
 
 if (currentTheme === "dark") {
@@ -9,11 +10,13 @@ if (currentTheme === "dark") {
 
 buttonThemeToggle.addEventListener("click", () => {
         let theme = "light";
-        buttonThemeToggle.setAttribute("aria-pressed", false)
+        buttonThemeToggle.setAttribute("aria-pressed", false);
+        buttonThemeToggleIcon.setAttribute("class", "fa-soild fa-moon");
 
         if (document.documentElement.getAttribute("data-theme") !== "dark") {
-            theme = "dark"
-            buttonThemeToggle.setAttribute("aria-pressed", true)
+            theme = "dark";
+            buttonThemeToggle.setAttribute("aria-pressed", true);
+            buttonThemeToggleIcon.setAttribute("class", "fa-regular fa-sun");
         }
 
         document.documentElement.setAttribute("data-theme", theme);
